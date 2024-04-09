7cf9d6d2481759cff18a398d7ea99b236648c9ae
Anthonyvillaaa <161419583+Anthonyvillaaa@users.noreply.github.com>
2024-04-08 20:05:18 -0700

    updates

--- NFA design practice.py
+++ NFA design practice.py
@@ -28,6 +28,7 @@ class NFA:
 
 
 digits = {'0','1','2','3','4','5','6','7','8','9'}
+octa_digits = {'0','1','2','3','4','5','6','7'}
 alphabet = {'_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
 hex_letters = {'b','B'}
 octa_letters = {'o','O'}
@@ -43,15 +44,16 @@ N0 = NFA({0,1,2},
 	 {(0,'0'): {7,8} } | {(7, letter): {7,8} for letter in hex_letters} | {(8, nums): {8} for nums in binaryNums} |
 	 {(8, '_'): {9}} |{(9, nums):{10,8} for nums in binaryNums} | {(10, nums):{10,8} for nums in binaryNums}| 			# NFA for Hexinteger
 
-	 {(0,'0'): {11,12}} | {(11, letters): {11, 12} for letters in octa_letters} | {(12, nums): {12} for nums in binaryNums} | {(12, '_'): {13}} |
-	 {(13, nums): {14, 12} for nums in binaryNums} | {(14, nums): {14, 12} for nums in binaryNums} ,				# NFA for Octinteger
+	 {(0,'0'): {11}} | {(11, letters): {12} for letters in octa_letters} | {(12, nums): {12,14} for nums in octa_digits} | {(12, '_'): {13}} |
+	 {(13, nums): {14, 12} for nums in octa_digits} | {(14, nums): {14, 12} for nums in octa_digits} ,				# NFA for Octinteger
 	 
 
 	 {0},
-	 {1,3,6,8,10,12,14})
-	 
-	
+
+	 {1, 3, 6, 8, 10, 14})
+		 
+		
 
 if __name__ == "__main__":
-    input_string = input("Please enter valid decimal integer: ")
-    print(N0.run(input_string))
+	input_string = input("Please enter valid integer: ")
+	print(N0.run(input_string))
